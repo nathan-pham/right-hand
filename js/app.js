78e16ba126374dbd2eff05ddf3c2421d30f802eb
Nathan Pham <nathanpham.me@gmail.com>
2022-04-25 19:34:38 -0700

    icon components

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import Sortable from "sortablejs";
+
+import HandleIcon from "./components/icons/HandleIcon.js";
+import TrashIcon from "./components/icons/TrashIcon.js";
+document.body.appendChild(TrashIcon());
